de271d70fd7e6ac2d8419e917a75e20da41645a6
park jeong seob <wjdtjq9655@gmail.com>
2019-04-01 18:02:08 +0900

    socketioTest

---
+++ server.js
@@ -0,0 +1,28 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(3000);
+
+function  handler(req, res) {
+    fs.readFile('./index.html', function (err, data) {
+        if(err){
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    })
+}
+
+io.on('connection', function (socket) {
+   socket.emit('news', {serverData : "서버 작동"});
+
+   socket.on('client login', function (data) {
+       console.log(data)
+   });
+
+   socket.on('disconnect', function () {
+       console.log('접속이 종료되었습니다.');
+   });
+});
